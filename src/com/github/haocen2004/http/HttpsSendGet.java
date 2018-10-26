d4c01633db555a1ff1fd3b54d63f6e0cb5481e5a
haocen.minecraft@qq.com <haocen.minecraft@qq.com>
2018-10-26 21:59:19 +0800

    beta-0.1-update code to read

--- src/com/github/haocen2004/http/HttpsSendGet.java
+++ src/com/github/haocen2004/http/HttpsSendGet.java
@@ -10,61 +10,60 @@ import java.util.Map;
 public class HttpsSendGet {
 
 	public static String send(String url, String param) {
-		
+
 		String result = "";
-        BufferedReader in = null;
-        
-        try {
-			
-            String urlNameString = url + "?" + param;
-            URL realUrl = new URL(urlNameString);
-            URLConnection connection = realUrl.openConnection();
-            connection.setRequestProperty("accept", "*/*");
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            connection.connect();
-            Map<String, List<String>> map = connection.getHeaderFields();
+		BufferedReader in = null;
 
-            for (String key : map.keySet()) {
-				
-                System.out.println(key + "--->" + map.get(key));
-                
-            }
+		try {
 
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            
-            while ((line = in.readLine()) != null) {
-				
-                result += line;
-                
-            }
-        } catch (Exception e) {
-			
-            System.out.println("Error!" + e);
-            e.printStackTrace();
-            
-        } 
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			URLConnection connection = realUrl.openConnection();
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.connect();
+			Map<String, List<String>> map = connection.getHeaderFields();
 
-        finally {
-			
-            try {
-				
-                if (in != null) {
-                    in.close();
-                    
-                }
-                
-            } catch (Exception e2) {
-				
-                e2.printStackTrace();
-                
-            }
-        }
-        
-        return result;
-        
-    }
-	
+			for (String key : map.keySet()) {
+
+				System.out.println(key + "--->" + map.get(key));
+
+			}
+
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+
+			while ((line = in.readLine()) != null) {
+
+				result += line;
+
+			}
+		} catch (Exception e) {
+
+			System.out.println("Error!" + e);
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+			try {
+
+				if (in != null) {
+					in.close();
+
+				}
+
+			} catch (Exception e2) {
+
+				e2.printStackTrace();
+
+			}
+		}
+
+		return result;
+
+	}
+
 }
