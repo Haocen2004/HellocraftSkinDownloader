41118133bd59aae9f01ed180bfe59649cf9a1d0d
haocen.minecraft@qq.com <haocen.minecraft@qq.com>
2018-11-03 13:13:52 +0800

    add icon

--- src/com/github/haocen2004/ui/Launcher.java
+++ src/com/github/haocen2004/ui/Launcher.java
@@ -3,6 +3,7 @@ package com.github.haocen2004.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -22,6 +23,7 @@ public class Launcher extends Application {
 					FXMLLoader.load(getClass().getResource("/resources/assets/fxml/UImain.fxml")));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("/resources/assets/img/icon.png"));
 			primaryStage.setTitle("Hellocraft Skins Downloader");
 			primaryStage.show();
 
